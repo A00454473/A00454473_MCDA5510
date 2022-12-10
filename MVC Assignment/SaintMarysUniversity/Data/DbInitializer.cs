41b9a63c72af039d9d32c5002e459e81e750e9ce
A00454473 <rishan.mascarenhas@smu.ca>
2022-12-10 12:46:05 -0400

    MVC Assignment added from repository https://github.com/A00454473/A00454473_MCDA5510_MVC_Assignment
    Signed-off-by: A00454473 <rishan.mascarenhas@smu.ca>

---
+++ MVC Assignment/SaintMarysUniversity/Data/DbInitializer.cs
@@ -0,0 +1,76 @@
+﻿using SaintMarysUniversity.Models;
+using SaintMarysUniversity.Data;
+using SaintMarysUniversity.Models;
+using System;
+using System.Linq;
+
+namespace SaintMarysUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Students.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var students = new Student[]
+            {
+            new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01"), Anumber="A748574"},
+            new Student{FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01"), Anumber="A647584"},
+            new Student{FirstMidName="Arturo",LastName="Anand",EnrollmentDate=DateTime.Parse("2003-09-01"), Anumber = "A857467"},
+            new Student{FirstMidName="Gytis",LastName="Barzdukas",EnrollmentDate=DateTime.Parse("2002-09-01"), Anumber = "A647388"},
+            new Student{FirstMidName="Yan",LastName="Li",EnrollmentDate=DateTime.Parse("2002-09-01"), Anumber = "A784657"},
+            new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01"), Anumber = "A090989"},
+            new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01"), Anumber = "A234536"},
+            new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01"), Anumber = "A453674"}
+            };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+            new Course{CourseID=1050,Title="Chemistry",Credits=3, CourseNumber=3288, CRN=8901},
+            new Course{CourseID=4022,Title="Microeconomics",Credits=3, CourseNumber=4322, CRN=8902},
+            new Course{CourseID=4041,Title="Macroeconomics",Credits=3, CourseNumber=4543, CRN=8903},
+            new Course{CourseID=1045,Title="Calculus",Credits=4, CourseNumber=6555, CRN=8900},
+            new Course{CourseID=3141,Title="Trigonometry",Credits=4, CourseNumber=3454, CRN=8904},
+            new Course{CourseID=2021,Title="Composition",Credits=3, CourseNumber=8686, CRN=8905},
+            new Course{CourseID=2042,Title="Literature",Credits=4, CourseNumber=8786, CRN=8906}
+            };
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=6,CourseID=1045},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+            foreach (Enrollment e in enrollments)
+            {
+                context.Enrollments.Add(e);
+            }
+            context.SaveChanges();
+        }
+    }
+}
